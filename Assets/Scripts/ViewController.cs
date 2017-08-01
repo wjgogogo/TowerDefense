a29d58bde2c970423f042954a0313f15effb73c2
XvWenJun <906626481@qq.com>
2017-08-01 22:46:14 +0800

    摄像机移动完成

---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour {
+
+    public float speed = 4;
+    public float mouseSpeed = 20;
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float mouse = Input.GetAxisRaw("Mouse ScrollWheel");
+        Debug.Log(mouse);
+        transform.Translate(new Vector3(h*speed, mouse*mouseSpeed, v*speed)*Time.deltaTime,Space.World);
+
+       
+	}
+}

023867759766281f6239f4e092135a8786e1cebb
XvWenJun <906626481@qq.com>
2017-08-02 11:44:12 +0800

    炮台ui选择完成

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public TurretData laserTurretData;
+    public TurretData missileTurretData;
+    public TurretData standardTurretData;
+
+    private TurretData selectTurretData;
+
+    public void OnLaserSelect(bool isOn)
+    {
+        if (isOn)
+        {
+            selectTurretData = laserTurretData;
+        }
+    }
+
+    public void OnMissileSelect(bool isOn)
+    {
+        if (isOn)
+        {
+            selectTurretData = missileTurretData;
+        }
+    }
+
+    public void OnStandardSelect(bool isOn)
+    {
+        if (isOn)
+        {
+            selectTurretData = standardTurretData;
+        }
+    }
+}
---
+++ Assets/Scripts/TurretData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TurretData
+{
+    public GameObject turretPrefab;
+    public GameObject turetPrefabUpgrade;
+    public int cost;
+    public int costUpgrade;
+    public TurretType type;
+}
+
+public enum TurretType
+{
+    LaserTurret,
+    MissileTurret,
+    StandardTurret
+}

bfa4a7cba8a9b9634fe8b48ad2867cf3a8fbf134
XvWenJun <906626481@qq.com>
2017-08-02 12:56:53 +0800

    炮台生成及生成特效完成

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour
 {
+    public int money = 1000;
+    public Text moneyText;
+    public Animator moneyAnim;
     public TurretData laserTurretData;
     public TurretData missileTurretData;
     public TurretData standardTurretData;
@@ -31,6 +36,44 @@ public class BuildManager : MonoBehaviour
         if (isOn)
         {
             selectTurretData = standardTurretData;
+        }
+    }
+
+    private void ChangeMoney(int change = 0)
+    {
+        money += change;
+        moneyText.text = "￥" + money;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                bool ishit = Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("MapCube"));
+                if (ishit && selectTurretData != null)
+                {
+                    MapCube mapCube = hit.collider.GetComponent<MapCube>();
+                    if (mapCube.turrentGo == null)
+                    {
+                        if (money >= selectTurretData.cost)
+                        {
+                            ChangeMoney(-selectTurretData.cost);
+                            mapCube.BuildTurret(selectTurretData.turretPrefab);
+                        }
+                        else
+                        {
+                            moneyAnim.SetTrigger("Filck");
+                        }
+                    }
+                    else
+                    {
+                    }
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/MapCube.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCube : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject turrentGo;
+
+    public GameObject buildEffect;
+
+    public void BuildTurret(GameObject turret)
+    {
+        Instantiate(turret, transform.position, Quaternion.identity);
+        turrentGo = turret;
+        GameObject effect = Instantiate(buildEffect, transform.position, Quaternion.identity) as GameObject;
+        Destroy(effect, 0.5f);
+    }
+}
